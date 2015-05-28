dc4108186568f74ab6634979856fdbd9c4b26955
Boris Nagaev <bnagaev@gmail.com>
2015-05-28 19:31:18 +0300

    compile in debug mode

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 git submodule update --init
 luajit ./dynasm/dynasm.lua -D X64 -o src/lpjit_compile_posix64.c src/lpjit_compile.c
-luarocks make --local LPEG_INCDIR=lpeg
+luarocks make --local LPEG_INCDIR=lpeg CFLAGS='-g -O0 -shared -fPIC'
