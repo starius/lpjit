2aa391a248ea1ecb3d977082d25eb927137d813b
Boris Nagaev <bnagaev@gmail.com>
2015-06-03 00:23:42 +0300

    link minilua with -lm
    https://travis-ci.org/starius/lpjit/jobs/65152730#L299

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 git submodule update --init
 
 if [ ! -f minilua ]; then
-    gcc -o minilua luajit-2.0/src/host/minilua.c
+    gcc -o minilua luajit-2.0/src/host/minilua.c -lm
 fi
 
 ./minilua ./dynasm/dynasm.lua -D X64 -o src/lpjit_compile_posix64.c src/lpjit_compile.c
