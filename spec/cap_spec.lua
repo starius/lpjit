42b1ecfd6450c35977d922e9b1d07160c988a6fa
Boris Nagaev <bnagaev@gmail.com>
2015-06-03 02:24:41 +0300

    add pending test to cap_spec.lua
    This test results either in SEGFAULT or in occupying all
    available RAM. The source of this bug seems to be the code
    calling a function (lpjit_doubleCap).

--- spec/cap_spec.lua
+++ spec/cap_spec.lua
@@ -9,4 +9,25 @@ describe("lpjit", function()
         assert.equal('b', pattern2:match('abc'))
         assert.falsy(pattern2:match('ff'))
     end)
+
+    local allchar = {}
+    for i = 0, 255 do
+        allchar[i + 1] = i
+    end
+    allchar = string.char(unpack(allchar))
+    assert(#allchar == 256)
+
+    pending("can grow list of captures", function()
+        local lpeg = require 'lpeg'
+        local lpjit = require 'lpjit'
+
+        local pattern = lpeg.Cs((lpeg.R'09' + lpeg.P(1)/"")^0)
+
+        lpeg.pcode(pattern)
+
+        local pattern2 = lpjit.compile(pattern)
+        for i = 1, 1000 do
+            assert.equal('0123456789', pattern2:match(allchar))
+        end
+    end)
 end)
