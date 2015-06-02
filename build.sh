e1f401c0e1475586d7cbe2c84061da74c74faff0
Boris Nagaev <bnagaev@gmail.com>
2015-06-03 00:06:08 +0300

    build.sh: compile minilua; run dynasm.lua with it
    Other options are:
     * luajit
     * lua + a module providing "bit"

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 git submodule update --init
-luajit ./dynasm/dynasm.lua -D X64 -o src/lpjit_compile_posix64.c src/lpjit_compile.c
+
+if [ ! -f minilua ]; then
+    gcc -o minilua luajit-2.0/src/host/minilua.c
+fi
+
+./minilua ./dynasm/dynasm.lua -D X64 -o src/lpjit_compile_posix64.c src/lpjit_compile.c
 luarocks make --local LPEG_INCDIR=lpeg CFLAGS='-g -O0 -shared -fPIC'
