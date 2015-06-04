89182c64659b886dad52b65d4cf15281ebc382c9
Boris Nagaev <bnagaev@gmail.com>
2015-06-04 16:04:08 +0300

    some tests for lpeg wrapper
    Other tests are in test.lua (lpeg), not yet passed.

---
+++ spec/lpeg_spec.lua
@@ -0,0 +1,19 @@
+describe("lpjit.lpeg", function()
+    it("has enough stack size", function()
+        lpeg.setmaxstack(5)
+        local m = require 'lpjit.lpeg'
+        local b = {[1] =
+            "(" * (((1 - m.S"()") + #m.P"(" * m.V(1))^0) * ")"
+        }
+        local p1 = (#m.P(b) * 1) * m.Cp()
+        local p2 = m.P {
+            [1] = p1 + (1 * m.V(1))
+        }
+        assert.equal(7, m.match(p2, "  (  (a)"))
+    end)
+
+    it("pass same number of args to impl", function()
+        local m = require 'lpjit.lpeg'
+        assert.equal(nil, m.match(m.Cc(nil), ""))
+    end)
+end)
